4672ed9e743d6118b6099f7c83c13a36869ecb56
Sergey Sbitnev <sergeysbitnev@mail.ru>
2021-01-26 22:44:26 +0300

    Finish Project1_1B

--- Project 1_1B/Project1_1B.py
+++ Project 1_1B/Project1_1B.py
@@ -1,4 +1,4 @@
-"""
+
 cost = [20, 70, 90, 54, 41, 27, 11, 684, 74, 102]
 price = []
 
@@ -10,7 +10,8 @@ for item in cost:
 print('\nСтоимость с наценкой:', end=' ')
 for item in price:
 	print(item, end=' ')
-"""
+
+print('\n')
 from random import randint
 cost2 = [randint(1, 100) for i in range(1,10)]
 price2 = list(map(lambda item: item + item*0.2, cost2))
