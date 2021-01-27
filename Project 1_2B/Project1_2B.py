4224b8f9bd8d02629315d63dd4b5be05924a10e6
Sergey Sbitnev <sergeysbitnev@mail.ru>
2021-01-27 22:14:01 +0300

    Finish Project1_2B

---
+++ Project 1_2B/Project1_2B.py
@@ -0,0 +1,14 @@
+from random import randint
+name = ['молоко', 'кофе', 'хлеб', 'печенье', 'огурцы', 'сахар', 'яйца', 'мыло', 'соль', 'морковь']
+cost = [randint(20, 100) for i in range(0,10)]
+employee = [randint(100, 999) for i in range(0,10)]
+
+unite = list(zip(name, cost, employee))
+
+print('Товар\t\tЦена\t\tКод продавца', end=' ')
+for item in unite:
+	print()
+	for itemIn in item:
+		print(itemIn, end='\t\t')
+	
+
